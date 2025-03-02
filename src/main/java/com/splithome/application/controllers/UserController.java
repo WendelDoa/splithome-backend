fb94c3acab65deb9ed13054156693e383eaec95f
Wendel <sswendel11@outlook.com>
2025-03-02 02:13:57 -0300

    Ajustes de modelos e integração do front

--- src/main/java/com/splithome/application/controllers/UserController.java
+++ src/main/java/com/splithome/application/controllers/UserController.java
@@ -4,16 +4,16 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.splithome.application.entities.User;
 import com.splithome.application.services.UserService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
@@ -26,6 +26,16 @@ public class UserController {
             return new ResponseEntity<>(retorno, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/listall")
+    private ResponseEntity<List<User>> getAllUsers() {
+        try {
+            List<User> users = this.userService.getAllUsers();
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/com/splithome/application/services/UserService.java
+++ src/main/java/com/splithome/application/services/UserService.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Service;
 import com.splithome.application.entities.User;
 import com.splithome.application.repositories.UserRepository;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
     public String saveUser(User user) {
         userRepository.save(user);
